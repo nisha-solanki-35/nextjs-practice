09c0ba7932101907a6a4b486323a8288f8303187
yudiz_nisha <nisha.s@yudiz.com>
2021-10-15 15:50:30 +0530

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const about = () => {
+    return (
+        <div>
+            This is about us page
+            <Link href='/'>Back to home</Link>
+        </div>
+    )
+}
+
+export default about
---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const contact = () => {
+    return (
+        <div>
+            This is contact us page
+            <Link href='/'>Back to home</Link>
+        </div>
+    )
+}
+
+export default contact
